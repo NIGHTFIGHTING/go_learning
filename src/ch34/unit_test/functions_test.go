36ebb01b135dd19c68c497323e9a36fdcdca6d86
liuqi <night_fightina@163.com>
2020-01-21 15:44:09 +0800

    add

---
+++ src/ch34/unit_test/functions_test.go
@@ -0,0 +1,41 @@
+package functions_testing
+
+import (
+	"fmt"
+    "ch34/unit_test"
+    "testing"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestSquare(t *testing.T) {
+	inputs := [...]int{1, 2, 3}
+	expected := [...]int{1, 4, 9}
+	for i := 0; i < len(inputs); i++ {
+		ret := functions.square(inputs[i])
+		if ret != expected[i] {
+			t.Errorf("input is %d, the expected is %d, the actual %d",
+				inputs[i], expected[i], ret)
+		}
+	}
+}
+
+func TestErrorInCode(t *testing.T) {
+	fmt.Println("Start")
+	t.Error("Error")
+	fmt.Println("End")
+}
+
+func TestFailInCode(t *testing.T) {
+	fmt.Println("Start")
+	t.Fatal("Error")
+	fmt.Println("End")
+}
+
+func TestSquareWithAssert(t *testing.T) {
+	inputs := [...]int{1, 2, 3}
+	expected := [...]int{1, 4, 9}
+	for i := 0; i < len(inputs); i++ {
+		ret := functions.square(inputs[i])
+		assert.Equal(t, expected[i], ret)
+	}
+}

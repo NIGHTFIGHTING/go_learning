13c50b0fecfaa6b84ed679032d6cf1dbd3863cd4
liuqi <night_fightina@163.com>
2020-02-02 14:30:37 +0800

    recover

---
+++ src/errhandling/recover/recover.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+    //"errors"
+    "fmt"
+)
+
+func tryRecover() {
+    defer func() {
+        r := recover()
+        if err, ok := r.(error); ok {
+            fmt.Println("Error occurred:", err)
+        } else {
+            //panic(r)
+            panic(fmt.Sprintf(
+                "I don't know what to do: %v", r))
+            //panic: 123 [recovered]
+                //panic: I don't know what to do: 123
+        }
+    }()
+    //panic(errors.New("this is an error"))
+    //b := 0
+    //a := 5/b
+    //fmt.Println(a)
+    //Error occurred: runtime error: integer divide by zero
+
+    panic(123)
+}
+
+func main() {
+    tryRecover()
+}
